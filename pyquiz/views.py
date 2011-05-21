a1bc0f5ebba4e8ba60d34b5d57d13dc4d3f5b1ec
James Boisture <jamieboisture@gmail.com>
2011-05-21 15:09:33 -0400

    got the test creation form working.

--- pyquiz/views.py
+++ pyquiz/views.py
@@ -13,29 +13,61 @@ from deform import ValidationFailure
 from deform import Form
 from deform import widget
 
-class Answer(MappingSchema):
+class AnswerSchema(MappingSchema):
     text = SchemaNode(String())
     correct = SchemaNode(Boolean())
 
 class Answers(SequenceSchema):
-    answers = Answer()
+    answers = AnswerSchema()
 
-class Question(MappingSchema):
+class QuestionSchema(MappingSchema):
     text = SchemaNode(String())
     answers = Answers()
 
 class Questions(SequenceSchema):
-    questions = Question()
+    questions = QuestionSchema()
 
-class Tests(Schema):
+class TestSchema(Schema):
     name = SchemaNode(String())
     class_id = SchemaNode(String())
     questions = Questions()
 
 def test_form(request):
-    schema = Tests()
+    schema = TestSchema()
     myform = Form(schema, buttons=('submit',), use_ajax=True)
 
+    if 'submit' in request.POST: # detect that the submit button was clicked
+        controls = request.POST.items()
+        dbsession = DBSession()
+        running = True
+        foundTest = False
+        foundQuestion = False
+        c = 0
+        while running:
+            if controls[c][0] == "name" and controls[c+1][0] == "class_id":
+                testname = str(controls[c][1])
+                class_id = str(controls[c+1][1])
+                newTest = Test(testname, class_id)
+                dbsession.add(newTest)
+                dbsession.flush()
+                foundTest = True
+            if foundTest:
+                if (controls[c][0] == "__start__" and controls[c][1] == u'questions:mapping'):
+                    foundQuestion = True
+                    questionText = str(controls[c+1][1])
+                    question = Question(True, "Multiple_Choice", questionText, newTest.id)
+                    dbsession.add(question)
+                    dbsession.flush()
+                    foundQuestion = True
+                if controls[c] == ('__start__', u'answers:mapping'):
+                    answerText = str(controls[c+1][1])
+                    if controls[c+2][0] == 'correct': correct = True
+                    else: correct = False
+                    answer = Answer(question.id, answerText, correct)
+                    dbsession.add(answer)
+                    dbsession.flush
+            if controls[c][0] == 'submit': running = False
+            c += 1
     return {'form':myform.render()}
 
 def my_view(request):
